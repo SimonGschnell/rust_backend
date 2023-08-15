30d27f8b3f30a46f4db1029eca527bf438b424d4
simon_gschnell <simon.gschnell@gmail.com>
2023-08-15 11:56:37 +0200

    errors, Either and custom Responders

--- src/routes.rs
+++ src/routes.rs
@@ -4,7 +4,8 @@ mod scopes;
 
 pub fn routes(cfg: &mut web::ServiceConfig) {
     cfg.service(handlers::path_route)
-        .service(handlers::post_route);
+        .service(handlers::post_route)
+        .service(handlers::error_respose);
     cfg.service(web::scope("/test").configure(scopes::test_scope));
     cfg.service(web::scope("/counter").configure(scopes::counter_scope));
     cfg.service(web::scope("/car").configure(scopes::car_scope));
--- src/routes/handlers.rs
+++ src/routes/handlers.rs
@@ -1,6 +1,11 @@
-use actix_web::{get, web, Responder};
+use std::fmt::Debug;
+
+use actix_web::{
+    body::BoxBody, get, http::header::ContentType, web, Either, Error, HttpResponse, Responder,
+    ResponseError,
+};
 pub mod models;
-use serde::Deserialize;
+use serde::Serialize;
 
 pub async fn index_counter(data: web::Data<models::Counter>) -> impl Responder {
     let mut counter = data.value.lock().unwrap();
@@ -41,4 +46,75 @@ pub async fn post_req(data: web::Json<models::PostRequest>) -> impl Responder {
 //web::Query extractor is used for QUERY parameters in the request
 pub async fn query_req(data: web::Query<models::PostRequest>) -> impl Responder {
     format!("the following car was send: {:?}", data)
+}
+
+//? HttpResponse with custom type
+//* add the derive trait Serialize to the custom type
+#[derive(Serialize, Debug)]
+struct Custom {
+    address: String,
+    valid: bool,
+}
+
+//* implement the Responder trait for the custom type
+impl Responder for Custom {
+    type Body = BoxBody;
+    fn respond_to(self, _: &actix_web::HttpRequest) -> actix_web::HttpResponse<Self::Body> {
+        let body = serde_json::to_string(&self).unwrap();
+        HttpResponse::Ok()
+            .content_type(ContentType::json())
+            .body(body)
+    }
+}
+
+//* now we are able to return the custom type freely
+pub async fn return_custom_type() -> impl Responder {
+    Custom {
+        address: String::from("192.168.0.1"),
+        valid: true,
+    }
+}
+
+//? handler with that can return different HttpResponses
+
+type EitherResult = Either<HttpResponse, Result<String, actix_web::Error>>;
+async fn multiple_responses(data: web::Data<bool>) -> EitherResult {
+    let data = data.into_inner();
+    if *data {
+        Either::Left(HttpResponse::BadRequest().body("Bad request"))
+    } else {
+        Either::Right(Ok(String::from("OK")))
+    }
+}
+
+//? Error handling
+#[derive(Debug)]
+pub struct CustomError(String);
+
+impl std::fmt::Display for CustomError {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{}", self.0)
+    }
+}
+
+impl std::error::Error for CustomError {}
+impl ResponseError for CustomError {
+    fn status_code(&self) -> actix_web::http::StatusCode {
+        actix_web::http::StatusCode::FORBIDDEN
+    }
+    fn error_response(&self) -> HttpResponse<BoxBody> {
+        HttpResponse::build(self.status_code())
+            .content_type("text/plain")
+            .body(String::from(&self.0) + " => is the message of the error that i customly created")
+    }
+}
+
+#[get("/error/{bool}")]
+pub async fn error_respose(data: web::Path<bool>) -> Result<impl Responder, CustomError> {
+    let data = data.into_inner();
+    if data {
+        Ok("OK")
+    } else {
+        Err(CustomError(String::from("custom error")))
+    }
 }
